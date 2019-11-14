8481e0359287bd0ac2a2d860945338380bd298de
Sarfraz Ahmed <sarfraz@onsupport.com>
2019-11-14 12:47:32 +0500

    updated

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -19,8 +19,6 @@ try {
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
-/*
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
@@ -31,8 +29,6 @@ if (token) {
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
-*/
-
 
 import Echo from 'laravel-echo';
 
