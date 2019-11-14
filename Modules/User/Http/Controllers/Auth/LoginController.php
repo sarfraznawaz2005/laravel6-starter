8c1d6d0e4c524e9de2a30b6884f4fc2302688f28
Sarfraz Ahmed <sarfraz@onsupport.com>
2019-11-14 14:23:12 +0500

    updated

--- Modules/User/Http/Controllers/Auth/LoginController.php
+++ Modules/User/Http/Controllers/Auth/LoginController.php
@@ -40,7 +40,8 @@ class LoginController extends CoreController
         // If the class is using the ThrottlesLogins trait, we can automatically throttle
         // the login attempts for this application. We'll key this by the username and
         // the IP address of the client making these requests into this application.
-        if ($this->hasTooManyLoginAttempts($request)) {
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
             $this->fireLockoutEvent($request);
 
             return $this->sendLockoutResponse($request);
