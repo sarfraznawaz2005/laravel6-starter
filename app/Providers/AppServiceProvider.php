b43a0f41fe7f7c75ac8062803291ab99a8fc09a6
Sarfraz Ahmed <sarfraz@onsupport.com>
2019-11-25 18:44:14 +0500

    updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        app()->extend('command.controller.make', static function () {
-            exit('Controller creation disabled, use Actions instead!');
-        });
+        //
     }
 }
